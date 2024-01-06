f972dbe093d3141ad77682b6c3758d6bdfdfe253
Thomas Zachary <thomaszachary06@proton.me>
2024-01-06 13:34:02 +0000

    Changed visibility of fields in "View"

--- wordhuntsolver/gui/view.py
+++ wordhuntsolver/gui/view.py
@@ -9,54 +9,50 @@ from PIL.ImageTk import PhotoImage
 class View(tk.Tk):
     """The MVC view for the GUI"""
 
-    root: tk.Tk
-    photo_label: tk.Label
-    next_button: tk.Button
-    word_label: tk.Label
-    set_wordlist_button: tk.Button
-    set_image_button: tk.Button
-    status_label: tk.Label
+    _photo_label: tk.Label
+    _next_button: tk.Button
+    _word_label: tk.Label
+    _set_wordlist_button: tk.Button
+    _set_image_button: tk.Button
+    _status_label: tk.Label
 
-    status_text: tk.StringVar
+    _status_text: tk.StringVar
+    _image_ref: PhotoImage
 
     on_next: Callable[[], None] = lambda: None
     on_new_wordlist_path: Callable[[str], None] = lambda _: None
     on_new_image_path: Callable[[str], None] = lambda _: None
 
-    image_ref: PhotoImage
-
     def __init__(self):
         super().__init__()
 
-        self.geometry("500x500")
+        self._status_text = tk.StringVar()
 
-        self.status_text = tk.StringVar()
-
-        self.photo_label = tk.Label(self)
-        self.word_label = tk.Label(self)
-        self.next_button = tk.Button(
+        self._photo_label = tk.Label(self)
+        self._word_label = tk.Label(self)
+        self._next_button = tk.Button(
             self,
             text="Next",
             command=self._on_next
         )
-        self.set_wordlist_button = tk.Button(
+        self._set_wordlist_button = tk.Button(
             self,
             text="Set word list",
             command=self._on_new_wordlist_path
         )
-        self.set_image_button = tk.Button(
+        self._set_image_button = tk.Button(
             self,
             text="Set image",
             command=self._on_new_image_path
         )
-        self.status_label = tk.Label(self, textvariable=self.status_text)
+        self._status_label = tk.Label(self, textvariable=self._status_text)
 
-        self.photo_label.pack()
-        self.word_label.pack()
-        self.status_label.pack()
-        self.next_button.pack()
-        self.set_wordlist_button.pack()
-        self.set_image_button.pack()
+        self._photo_label.pack()
+        self._word_label.pack()
+        self._status_label.pack()
+        self._next_button.pack()
+        self._set_wordlist_button.pack()
+        self._set_image_button.pack()
 
     def set_image(self, image: Image, word: str):
         """Changes the image displayed on the GUI"""
@@ -65,14 +61,14 @@ class View(tk.Tk):
 
         photo = PhotoImage(resized)
 
-        self.word_label.config(text=word)
-        self.photo_label.config(image=photo)
-        self.image_ref = photo
+        self._word_label.config(text=word)
+        self._photo_label.config(image=photo)
+        self._image_ref = photo
 
     def set_status(self, text: str):
         """Sets the status displayed to the user"""
 
-        self.status_text.set(text)
+        self._status_text.set(text)
 
     def clear_status(self):
         """Clears status displayed to user"""
