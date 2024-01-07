3546dc83a62219f055f4b2c7bbb77eb21d77ef2d
Thomas Zachary <thomaszachary06@proton.me>
2024-01-07 13:33:18 +0000

    Improved configuration

--- wordhuntsolver/__main__.py
+++ wordhuntsolver/__main__.py
@@ -1,6 +1,5 @@
 """Main"""
 
-import sys
 import tomllib
 import pytesseract
 from wordhuntsolver.gui.controller import Controller
@@ -8,17 +7,28 @@ from wordhuntsolver.gui.view import View
 from wordhuntsolver.pather import Pather
 from wordhuntsolver.solver import Solver
 
+config = {
+    "tesseract": {
+        "command": "tesseract"
+    },
+    "word_list": {
+        "default_path": "word_list.txt"
+    }
+}
+
 def main():
     """Main function"""
-
-    with open("config.toml", "rb") as file:
-        data = tomllib.load(file)
-        pytesseract.pytesseract.tesseract_cmd = data["tesseract"]["command"]
 
     view = View()
     solver = Solver(Pather())
 
     controller = Controller(solver, view)
+
+    with open("config.toml", "rb") as file:
+        config.update(tomllib.load(file))
+
+        pytesseract.pytesseract.tesseract_cmd = config["tesseract"]["command"]
+        controller.load_word_list(config["word_list"]["default_path"])
 
     view.mainloop()
 
